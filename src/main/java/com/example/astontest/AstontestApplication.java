9192c1c86666fecd730dd75c0999ee04111da05d
Epremyan-Norik <epnorik@yandex.ru>
2023-09-11 15:44:24 +0300

    Initial commit

---
+++ src/main/java/com/example/astontest/AstontestApplication.java
@@ -0,0 +1,13 @@
+package com.example.astontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstontestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AstontestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/astontest/AstontestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.astontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstontestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
